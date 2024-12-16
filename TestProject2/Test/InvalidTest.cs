4ba6431b1105c58a6bb5acd7dd0c11fd83830704
tramntk <kimtramctt@gmail.com>
2024-12-16 20:17:07 +0700

    the second commit: very simple test framework

---
+++ TestProject2/Test/InvalidTest.cs
@@ -0,0 +1,38 @@
+﻿using ElectricCal;
+using Microsoft.Testing.Platform.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject2.Test
+{
+    [TestClass]
+    public class InvalidTest: BaseTest
+    {
+        [TestMethod("TC01: Verify message khi chi so dau ky lon hon chi so cuoi ky")]
+        public void TestChiSoCuoiKy_BeHonChiSoDauKy()
+        {
+            int chiSoDauKy = 10;
+            int chiSoCuoiKy = 1;
+
+            //Cach 1: handle = try catch
+            /*
+            //Khoi tao application
+            try
+            {
+                int actual = appTest.Calculator(chiSoDauKy, chiSoCuoiKy);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Chi so dau ky khong duoc lon hon chi so cuoi ki", ex.Message);
+            }
+            */
+
+            //Cách 2: handle = Assert
+            Assert.ThrowsException<Exception>(() => testApp.Calculator(chiSoDauKy, chiSoCuoiKy));
+            
+        }
+    }
+}
---
+++ TestProject2/Test/ValidTest.cs
@@ -0,0 +1,70 @@
+﻿using ElectricCal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject2.Test
+{
+    [TestClass]
+    public class ValidTest : BaseTest
+    {
+
+        //Verify case tinh gia tien bac 1
+        [TestMethod("TC1: Verify tinh gia tien bac 1")]
+        public void TinhTienBac1()
+        {
+            //Khai bao chi so dau ky, chi so cuoi ky
+            int chiSoDauKy = 0;
+            int chiSoCuoiKy = 1;
+             
+            //Expect value cho gia tien phai tra
+            int expected = 1678;
+            
+            //Run test
+            int actual = testApp.Calculator(chiSoDauKy, chiSoCuoiKy);
+
+            //Kiem tra
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        //Verify case tinh gia tien bac 1
+        [TestMethod("TC3: Verify tinh gia tien bac 1 su dung DataRow")]
+        [DataRow(100,100, DisplayName = "Tinh tien bac 1: input 100 va 100")]
+        [DataRow(100, 101, DisplayName = "Tinh tien bac 1: input 100 va 101")]
+        [DataRow(100, 150, DisplayName = "Tinh tien bac 1: input 100 va 150")]
+        public void TinhTienBac1_DataRow(int chiSoDauKy,int chiSoCuoiKy)
+        {
+            //Expect value cho gia tien phai tra
+            int expected = (chiSoCuoiKy - chiSoDauKy)* 1678;
+
+            //Run test
+            int actual = testApp.Calculator(chiSoDauKy, chiSoCuoiKy);
+
+            //Kiem tra
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        //Verify case tinh gia tien bac 2
+        [TestMethod("TC2: Verify tinh gia tien bac 2")]
+        [DataRow(100, 100, DisplayName = "Tinh tien bac 1: input 100 va 151")]
+        [DataRow(100, 101, DisplayName = "Tinh tien bac 1: input 100 va 199")]
+        [DataRow(100, 150, DisplayName = "Tinh tien bac 1: input 100 va 200")]
+        public void TinhTienBac2_DataRow(int chiSoDauKy, int chiSoCuoiKy)
+        {
+            //Expect value cho gia tien phai tra
+            int expected = (chiSoCuoiKy - chiSoDauKy)* 1734;
+
+            //Run test
+            int actual = testApp.Calculator(chiSoDauKy, chiSoCuoiKy);
+
+            //Kiem tra
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
